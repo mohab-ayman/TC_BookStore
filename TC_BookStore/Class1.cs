4f0ca54bc15ab261d94d46358004bc72520a192e
MAyman <mayman@integrant.com>
2016-07-20 19:58:32 +0200

    test 1

--- TC_BookStore/Class1.cs
+++ TC_BookStore/Class1.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
 
 namespace TC_BookStore
 {
+    [TestFixture]
     public class Class1
     {
         //hello
+        [SetUp]
+        public void Start()
+        { }
+
+        [TearDown]
+        public void End()
+        { }
+
+        [Test]
+        public void OpenGoogle()
+        {
+            IWebDriver _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl("http://10.1.23.114/BookStore");
+        }
     }
 }
