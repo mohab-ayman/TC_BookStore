51354255fe1c02c5910c64c563ce6cbaf4d1ef9a
EFarrag <eman.f.abdelaziz@gmail.com>
2016-08-04 13:49:39 +0200

    Updates

--- TC_BookStore/TestInputData/RegisterationData.cs
+++ TC_BookStore/TestInputData/RegisterationData.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Excel = Microsoft.Office.Interop.Excel;
+using System.Data.OleDb;
+using System.Data;
+
 
 
 
@@ -12,5 +14,24 @@ namespace TC_BookStore.TestData
 {
     class RegisterationData
     {
+        public static string TestDataFileConnection()
+        {
+            var fileName = ConfigurationManager.AppSettings["TestDataSheetPath"];
+            var con = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source = {0}; Extended Properties=Excel 12.0;", fileName);
+            return con;
+        }
+
+        public static UserData GetTestData(string keyName)
+        {
+            using (var connection = new OleDbConnection(TestDataFileConnection()))
+            {
+                connection.Open();
+                var query = string.Format("select * from [DataSet$] where key='{0}'", keyName);
+                var value = connection.Query<UserData>(query).FirstOrDefault();
+                connection.Close();
+                return value;
+            }
+        }
     }
+}
 }
